6202fbb171ba076d5b630a9cfc459af137531e50
Elena050 <2008petrova.elena@gmail.com>
2025-12-19 11:11:06 +0300

    Update QestionDto.cs

--- TestingPlatform.Application/Dtos/QestionDto.cs
+++ TestingPlatform.Application/Dtos/QestionDto.cs
@@ -1,1 +1,10 @@
+namespace TestingPlatform.Application.Dtos;
 
+public class QuestionDto
+{
+    public int Id { get; set; }
+    public string Text { get; set; } = string.Empty;
+    public int Number { get; set; }
+    public string AnswerType { get; set; } = string.Empty;
+    public List<AnswerDto> Answers { get; set; } = new();
+}
