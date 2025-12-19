2ca9eded36d9a9b12a02a57bfd961b01f080a2e1
Elena050 <2008petrova.elena@gmail.com>
2025-12-19 11:12:12 +0300

    Update CreateQestionRequest.cs

--- TestingPlatform.Application/Dtos/CreateQestionRequest.cs
+++ TestingPlatform.Application/Dtos/CreateQestionRequest.cs
@@ -1,1 +1,17 @@
+using System.ComponentModel.DataAnnotations;
 
+namespace TestingPlatform.Application.Dtos;
+
+public class CreateQuestionRequest
+{
+    [Required]
+    public string Text { get; set; } = string.Empty;
+    
+    [Required]
+    public string AnswerType { get; set; } = "Single";
+    
+    [Range(1, 100)]
+    public int Number { get; set; }
+    
+    public List<CreateAnswerRequest> Answers { get; set; } = new();
+}
