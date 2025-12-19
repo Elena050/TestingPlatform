91da4633e0d9b1951786c72a3151ba1aeb5be749
Elena050 <2008petrova.elena@gmail.com>
2025-12-19 11:12:34 +0300

    Update CreateAnswerRequest.cs

--- TestingPlatform.Application/Dtos/CreateAnswerRequest.cs
+++ TestingPlatform.Application/Dtos/CreateAnswerRequest.cs
@@ -1,1 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 
+namespace TestingPlatform.Application.Dtos;
+
+public class CreateAnswerRequest
+{
+    [Required]
+    public string Text { get; set; } = string.Empty;
+    
+    public bool IsCorrect { get; set; }
+}
