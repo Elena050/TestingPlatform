ab5f15dc13d2f9817f732c043b28d1c5e1ac95a3
Elena050 <2008petrova.elena@gmail.com>
2025-12-19 11:11:24 +0300

    Update AnswerDto.cs

--- TestingPlatform.Application/Dtos/AnswerDto.cs
+++ TestingPlatform.Application/Dtos/AnswerDto.cs
@@ -1,1 +1,8 @@
+namespace TestingPlatform.Application.Dtos;
 
+public class AnswerDto
+{
+    public int Id { get; set; }
+    public string Text { get; set; } = string.Empty;
+    public bool IsCorrect { get; set; }
+}
